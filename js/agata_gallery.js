a3ab01c89d431e2ecd0c1525bcf5c979563ecf89
Lina Ribokaitė <ribok@Julija.(none)>
2018-01-17 18:34:37 +0200

    popup added

--- js/agata_gallery.js
+++ js/agata_gallery.js
@@ -1,6 +1,6 @@
 (function ($) {
   $(document).ready(function(){
-    $("#agata-gallery-videos").addClass('active');
+    $("#agata-gallery-images").addClass('active');
     
     $(".gallery-tab-link.video-tab").click(function(){
         $("#agata-gallery-videos").addClass('active');
@@ -9,6 +9,12 @@
     $(".gallery-tab-link.images-tab").click(function(){
         $("#agata-gallery-videos").removeClass('active');
         $("#agata-gallery-images").addClass('active');
+    });
+
+    $(".fancybox").fancybox({
+      beforeShow : function(){
+        this.title =  $(this.element).data("caption");
+      }
     });
     
   });
